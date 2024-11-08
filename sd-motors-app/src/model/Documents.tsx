9255baf675f98061d86cd69b01bf60112cf66e0f
Demesh Fernando <wdemeshfernando@gmail.com>
2024-11-08 07:21:46 +0530

    #11 Initial server setup

---
+++ sd-motors-app/src/model/Documents.tsx
@@ -0,0 +1,11 @@
+export default [
+  {
+    DocumentID: 3224,
+    DocumentName: "Nissan GTR",
+    CreatedDate: "2024-11-08T07:00:00",
+    UpdatedDate: "2024-11-08T07:00:00",
+    CreatedBy: 0,
+    UpdatedBy: 0,
+    InitialTemplate: "",
+  },
+];
---
+++ sd-motors-app/src/model/Staff.tsx
@@ -0,0 +1,15 @@
+export default [
+  {
+    ID: 1,
+    NIC: 22033945,
+    FirstName: "Hansika",
+    LastName: "Wijerathne",
+    Salutation: "Ms.",
+    CreatedDate: "2024-10-08T07:15:00",
+    UpdatedDate: "2024-10-08T07:15:00",
+    CreatedBy: 0,
+    UserName: "hWijerathne",
+    UserPassword: "1234",
+    Email: "email@gmail.com",
+  },
+];
